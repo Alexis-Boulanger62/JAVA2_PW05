3e11c4f4eada21ffc854e42f2e0c3683197e6d66
Alexis BOULANGER <alexis.boulanger@student.junia.com>
2026-02-08 13:05:19 +0100

    Movie DAO implemented

--- src/main/java/fr/isen/java2/db/daos/MovieDao.java
+++ src/main/java/fr/isen/java2/db/daos/MovieDao.java
@@ -1,20 +1,100 @@
 package fr.isen.java2.db.daos;
 
+import static fr.isen.java2.db.daos.DataSourceFactory.getDataSource;
+import fr.isen.java2.db.entities.Genre;
 import java.util.List;
 
 import fr.isen.java2.db.entities.Movie;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class MovieDao {
 
 	public List<Movie> listMovies() {
-		throw new RuntimeException("Method is not yet implemented");
+		List<Movie> listofMovies = new ArrayList<>();
+            try (Connection connection = getDataSource().getConnection()) {
+              try (PreparedStatement statement = 
+                        connection.prepareStatement("SELECT * FROM movie JOIN genre ON movie.genre_id = genre.idgenre")) {
+                   try (ResultSet results = statement.executeQuery()) {
+                      while (results.next()) {
+                          Genre genre = new Genre(results.getInt("idgenre"),
+                                            results.getString("name"));
+                          Movie movie = new Movie(results.getInt("idmovie"),
+                                            results.getString("title"),
+                                            results.getDate("release_date").toLocalDate(),
+                                            genre,
+                                            results.getInt("duration"),
+                                            results.getString("director"),
+                                            results.getString("summary"));
+                    listofMovies.add(movie);
+                } 
+                   }
+              } 
+               return listofMovies;
+            } catch (SQLException e){
+                throw new RuntimeException("oups",e);
+            }
 	}
 
 	public List<Movie> listMoviesByGenre(String genreName) {
-		throw new RuntimeException("Method is not yet implemented");
+		List<Movie> listofMovies = new ArrayList<>();
+            try (Connection connection = getDataSource().getConnection()) {
+              try (PreparedStatement statement = 
+                        connection.prepareStatement("SELECT * FROM movie JOIN genre ON movie.genre_id = genre.idgenre WHERE genre.name = ?")) {
+                  statement.setString(1, genreName);
+                   try (ResultSet results = statement.executeQuery()) {
+                      while (results.next()) {
+                          Genre genre = new Genre(results.getInt("idgenre"),
+                                            results.getString("name"));
+                          Movie movie = new Movie(results.getInt("idmovie"),
+                                            results.getString("title"),
+                                            results.getDate("release_date").toLocalDate(),
+                                            genre,
+                                            results.getInt("duration"),
+                                            results.getString("director"),
+                                            results.getString("summary"));
+                    listofMovies.add(movie);
+                } 
+                   }
+              } 
+               return listofMovies;
+            } catch (SQLException e){
+                throw new RuntimeException("oups",e);
+            }
 	}
 
 	public Movie addMovie(Movie movie) {
-		throw new RuntimeException("Method is not yet implemented");
+		 try (Connection connection = getDataSource().getConnection()) {
+                String sqlQuery = "INSERT INTO movie(title,release_date,genre_id,duration,director,summary) VALUES(?,?,?,?,?,?)";
+                try (PreparedStatement statement = connection.prepareStatement
+        (sqlQuery,Statement.RETURN_GENERATED_KEYS)) {
+                   statement.setString(1, movie.getTitle());
+                   statement.setDate(2, java.sql.Date.valueOf(movie.getReleaseDate()));
+                   statement.setInt(3,movie.getGenre().getId());
+                   statement.setInt(4,movie.getDuration());
+                   statement.setString(5, movie.getDirector());
+                   statement.setString(6, movie.getSummary());
+                   statement.executeUpdate();
+                   ResultSet ids = statement.getGeneratedKeys();
+                   if (ids.next()){
+                       return new Movie(
+                        ids.getInt(1),          
+                        movie.getTitle(),
+                        movie.getReleaseDate(),
+                        movie.getGenre(),
+                        movie.getDuration(),
+                        movie.getDirector(),
+                        movie.getSummary()
+                    );
+                   }
+                   return null;
+              } 
+            } catch (SQLException e){
+                throw new RuntimeException("oups",e);
+            }
 	}
 }
--- src/test/java/fr/isen/java2/db/daos/MovieDaoTestCase.java
+++ src/test/java/fr/isen/java2/db/daos/MovieDaoTestCase.java
@@ -1,14 +1,24 @@
 package fr.isen.java2.db.daos;
 
+import fr.isen.java2.db.entities.Genre;
+import fr.isen.java2.db.entities.Movie;
 import static org.assertj.core.api.Assertions.fail;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MovieDaoTestCase {
+        private final MovieDao movieDao = new MovieDao();
+        private final GenreDao genreDao = new GenreDao();
 	@BeforeEach
 	public void initDb() throws Exception {
 		Connection connection = DataSourceFactory.getDataSource().getConnection();
@@ -37,18 +47,82 @@ public class MovieDaoTestCase {
 		connection.close();
 	}
 	
-	 @Test
-	 public void shouldListMovies() {
-		 fail("Not yet implemented");
-	 }
+    @Test
+    public void shouldListMovies() {
+        // WHEN
+        List<Movie> movies = movieDao.listMovies();
+
+        // THEN
+        assertThat(movies).hasSize(3);
+
+        assertThat(movies)
+            .extracting(
+                "id",
+                "title",
+                "genre.name",
+                "duration"
+            )
+            .containsOnly(
+                tuple(1, "Title 1", "Drama", 120),
+                tuple(2, "My Title 2", "Comedy", 114),
+                tuple(3, "Third title", "Comedy", 176)
+            );
+    }
 	
 	 @Test
 	 public void shouldListMoviesByGenre() {
-		 fail("Not yet implemented");
+		// WHEN
+		List<Movie> movies = movieDao.listMoviesByGenre("Comedy");
+		// THEN
+		assertThat(movies).hasSize(2);
+		assertThat(movies)
+            .extracting(
+                "id",
+                "title",
+                "genre.name",
+                "duration"
+            )
+            .containsOnly(
+                tuple(2, "My Title 2", "Comedy", 114),
+                tuple(3, "Third title", "Comedy", 176)
+            );
 	 }
 	
 	 @Test
-	 public void shouldAddMovie() throws Exception {
-		 fail("Not yet implemented");
-	 }
+public void shouldAddMovie() throws Exception {
+        // GIVEN
+        Genre genre = new Genre(4,"Western"); 
+        Movie movie = new Movie(
+            "Funny Movie",
+            LocalDate.of(2023, 2, 8),
+            genre,
+            120,
+            "John Doe",
+            "A hilarious movie for testing"
+        );
+
+        // WHEN
+        Movie savedMovie = movieDao.addMovie(movie);
+
+        // THEN
+        Connection connection = DataSourceFactory.getDataSource().getConnection();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM movie WHERE title = 'Funny Movie'");
+        assertThat(resultSet.next()).isTrue();
+        assertThat(resultSet.getInt("idmovie")).isNotNull();
+        assertThat(resultSet.getString("title")).isEqualTo(movie.getTitle());
+        assertThat(resultSet.getDate("release_date").toLocalDate()).isEqualTo(movie.getReleaseDate());
+        assertThat(resultSet.getInt("genre_id")).isEqualTo(movie.getGenre().getId());
+        assertThat(resultSet.getInt("duration")).isEqualTo(movie.getDuration());
+        assertThat(resultSet.getString("director")).isEqualTo(movie.getDirector());
+        assertThat(resultSet.getString("summary")).isEqualTo(movie.getSummary());
+        assertThat(resultSet.next()).isFalse();
+
+        resultSet.close();
+        statement.close();
+        connection.close();
 }
+        
+        
+}
+
